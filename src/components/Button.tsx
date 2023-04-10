818f019ce69c3d96d3cfbad1a71d4f850d599f5d
Gustavo <gust4v0309@gmail.com>
2023-04-10 19:00:02 -0300

    Feat: create Button Component

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  text: string;
+  onClick?: () => void;
+  className?: string;
+}
+
+export function Button(props: Props) {
+  return <button onClick={props.onClick} className={props.className} >{props.text}</button>;
+
+}
