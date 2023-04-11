45febff0969fbbd2780df8dfd01d314155eed975
Gustavo <gust4v0309@gmail.com>
2023-04-11 20:58:21 -0300

    Feat:  add new props to PomodoroTimer component in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {PomodoroTimer} from './components/PomodoroTimer'
 function App() {
   return (
-    <div className="App">
-      <PomodoroTimer defaultPomodoroTime={1500} />
+    <div className="container">
+      <PomodoroTimer pomodoroTime={1500} shortRestTime={300} longRestTime={900} cycles={4}/>
     </div>
   );
 }
