ad19086718b054601d2d821cc530e6acee7fe8ce
Gustavo <gust4v0309@gmail.com>
2023-05-05 19:04:27 -0300

    Refactor: App.tsx 6-10

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import {PomodoroTimer} from './components/PomodoroTimer'
 function App() {
   return (
     <div className="container">
-      <PomodoroTimer pomodoroTime={1500} shortRestTime={300} longRestTime={900} cycles={4}/>
+      <PomodoroTimer
+      pomodoroTime={1500}
+      shortRestTime={300}
+      longRestTime={900}
+      cycles={4}/>
     </div>
   );
 }
